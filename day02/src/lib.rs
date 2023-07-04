7f7822bc3b5007f5db907f838f882ffa35617763
Rens Heddes <rens@easy-lms.com>
2023-07-04 09:46:06 +0200

    Make day2 more rusty

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,28 +1,58 @@
+use std::str::FromStr;
+
+enum Command {
+    Forward { n: isize },
+    Up { n: isize },
+    Down { n: isize },
+}
+
+#[derive(Debug)]
+struct ParseCommandError;
+
+impl FromStr for Command {
+    type Err = ParseCommandError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.split_once(" ") {
+            Some(("forward", x)) => Ok(Self::Forward { n: x.parse().unwrap() }),
+            Some(("up", x)) => Ok(Self::Up { n: x.parse().unwrap() }),
+            Some(("down", x)) => Ok(Self::Down { n: x.parse().unwrap() }),
+            Some(_) => Err(ParseCommandError),
+            None => Err(ParseCommandError),
+        }
+    }
+}
+
+fn handle_part_1(command: Command, (horizontal, depth): (isize, isize)) -> (isize, isize) {
+    match command {
+        Command::Forward { n } => (horizontal + n, depth),
+        Command::Down { n } => (horizontal, depth + n),
+        Command::Up { n } => (horizontal, depth - n),
+    }
+}
+
+fn handle_part_2(command: Command, (horizontal, depth, aim): (isize, isize, isize)) -> (isize, isize, isize) {
+    match command {
+        Command::Forward { n } => (horizontal + n, depth + n * aim, aim),
+        Command::Down { n } => (horizontal, depth, aim + n),
+        Command::Up { n } => (horizontal, depth, aim - n),
+    }
+}
 
 pub fn part1(input: &str) -> isize {
     let coords = input.lines()
-        .map(|line| line.split_once(" ").unwrap())
-        .fold((0, 0), |(h, d), (command, n)| {
-            match (command, n.parse::<isize>().unwrap()) {
-                ("forward", v) => (h + v, d),
-                ("down", v) => (h, d + v),
-                ("up", v) => (h, d - v),
-                _ => unreachable!(),
-            }
+        .map(|line| Command::from_str(line))
+        .fold((0, 0), |coords, command| {
+            handle_part_1(command.unwrap(), coords)
         });
     coords.0 * coords.1
 }
 
 pub fn part2(input: &str) -> isize {
     let coords = input.lines()
-        .map(|line| line.split_once(" ").unwrap())
-        .fold((0, 0, 0), |(horizontal, depth, aim), (command, n)| {
-            match (command, n.parse::<isize>().unwrap()) {
-                ("forward", v) => (horizontal + v, depth + (v * aim), aim),
-                ("down", v) => (horizontal, depth, aim + v),
-                ("up", v) => (horizontal, depth, aim - v),
-                _ => unreachable!(),
-            }
+        .map(|line| Command::from_str(line))
+        .fold((0, 0, 0), |coords, command| {
+            handle_part_2(command.unwrap(), coords)
         });
     coords.0 * coords.1
 }
